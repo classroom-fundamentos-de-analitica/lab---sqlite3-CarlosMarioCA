6be4769ad12f62349b4863c42cf6237f7b5aa3e6
carlosmarioca <camchavezag@unal.edu.co>
2023-06-05 21:29:18 -0500

    4

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -42,4 +42,4 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --
 
-SELECT YEAR(c23), AVG(c21) FROM tbl2 GROUP BY(YEAR(c23));
+SELECT YEAR(c23) as YEAR, AVG(c21) as "avg(c21)" FROM tbl2 GROUP BY(YEAR(c23));
