851c74541e308d0252e3c1e48f31fddf89df6484
carlosmarioca <camchavezag@unal.edu.co>
2023-06-05 21:27:11 -0500

    3

--- pregunta_04.sql
+++ pregunta_04.sql
@@ -40,4 +40,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT K0, c16 FROM tbl1 WHERE SUBSTRING(c16,1,1)  LIKE K0 + '0%'
+SELECT K0, c16 FROM tbl1 WHERE K0=SUBSTRING(c16,1,1) 
